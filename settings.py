b56c30a7854f7a11a697b8c5d6797a6c6dd73dbc
MTD <dev.mantendo@gmail.com>
2021-07-07 13:14:49 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+import os
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
