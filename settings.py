0020ef8aa2518fbe9c8005c6beca781dd2d2e76c
MTD <dev.mantendo@gmail.com>
2021-07-07 15:07:58 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 import os
+import django_heroku
 
+django_heroku.settings(locals())
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = (
