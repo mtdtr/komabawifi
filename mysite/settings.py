b1513484b2b4d9768e7155e60433cd97aa5a744a
MTD <dev.mantendo@gmail.com>
2021-07-07 15:23:59 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'blog/static/'),
+    os.path.join(BASE_DIR, "static"),
 )
 
 # Default primary key field type
