f9f1ba8f9943f7f9e3b4f77b749be4e72135e8e7
MTD <dev.mantendo@gmail.com>
2021-07-08 17:06:22 +0900

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -4,9 +4,7 @@ import json
 import urllib.request
 import sys
 
-option = 6
-
-#名前を調整する関数です
+#名前を調整する関数です。
 def name(nom):
   if "室" in nom:
     target = "室"
@@ -23,7 +21,7 @@ def name(nom):
   else:
     return nom
 
-def send(day, period):
+def send(day, period, option):
   if option == 0:
     return ""
   if day == option or option == 6:
@@ -40,18 +38,23 @@ def send(day, period):
   else:
     return ""
 
-def classroom(cle):
+def classroom(cle, option):
   class_result = ""
   for day in range(5):
     for period in range(5):
       keykey = str(day + 1) + "_" + str(period + 1)
       if keykey in cle:
-        class_result = class_result + str(send(day+1, period+1))
+        class_result = class_result + str(send(day+1, period+1, option))
   return class_result
 
 
 # Create your views here.
 def index(request):
+    if 'want_room' in request.GET:
+      option = int(request.GET['want_room'])
+    else:
+      option = 0
+
     #return HttpResponse("Hello, world. You're at the polls index.")
     #使用端末数のjsonファイルです。(当然ですが時間帯によって変化します)
     urllib.request.urlretrieve('https://wifi-monitor.nc.u-tokyo.ac.jp/map/data/ap-data2.json', 'data.json')
@@ -83,23 +86,28 @@ def index(request):
     #人数順にソート。
     nouveau_liste = sorted(nouveau_liste.items(), key=lambda x: x[1]['numClient'])
 
-    content = "<head><title>Komaba-Wifi-List</title></head><body><font color='#000000'>☆利用可能な教室を表示しますね!☆</font><br>"
+    content = "<head><title>Komaba-Wifi-List</title></head><body><form action='' method='get' class='wakaru'><select name='want_room'>"
+    content = content + "<option value='0'>授業情報を非表示</option><option value='1'>月</option><option value='2'>火</option><option value='3'>水</option><option value='4'>木</option><option value='5'>金</option><option value='6'>全表示</option></select>"
+    content = content.replace("value='" + str(option) + "'", "value='" + str(option) + "' selected")
+    content = content + "<input type='submit' value='送信'></form><font color='#000000'>☆利用可能な教室を表示しますね!☆</font><br>"
 
     for value in range(len(nouveau_liste)):
         for room in json_room:
             if json_room[room]['space_id'] == nouveau_liste[value][1]['space_id']:
                 if 'availability' not in json_room[room]: #"availability"がない=声出し可
                     room_name = name(json_room[room]['description'])
-                    """
+                    
                     if 'class' in json_room[room]:
-                        content = content + "<font color='#00aa00'>" + room_name + "　" + str(nouveau_liste[value][1]['numClient']) + "</font>　<font color='#aa0000'>" + classroom(json_room[room]['class']) + "</font><br>"
+                        content = content + "<font color='#00aa00'>" + room_name + "　</font>" + str(nouveau_liste[value][1]['numClient']) + "　<font color='#aa0000'>" + classroom(json_room[room]['class'], option) + "</font><br>"
                     else:
-                        content = content + "<font color='#00aa00'>" + room_name + "　" + str(nouveau_liste[value][1]['numClient']) + "</font><br>"
-                    """
-                    content = content + "<font color='#00aa00'>" + room_name + " " + str(nouveau_liste[value][1]['numClient']) + "</font><br>"
+                        content = content + "<font color='#00aa00'>" + room_name + "　</font>" + str(nouveau_liste[value][1]['numClient']) + "<br>"
+                    
+                    #content = content + "<font color='#00aa00'>" + room_name + "　</font>" + str(nouveau_liste[value][1]['numClient']) + "<br>"
                     break
                 elif json_room[room]['availability'] == "quiet": #静粛教室の情報
                     room_name = name(json_room[room]['description'])
-                    content = content + "<font color='#aa00aa'>" + room_name + "　" + str(nouveau_liste[value][1]['numClient']) + "</font><br>"
+                    content = content + "<font color='#aa00aa'>" + room_name + "　</font>" + str(nouveau_liste[value][1]['numClient']) + "<br>"
                     break
+
+    content = content + "緑が<font color='#00aa00'>声出し可</font>、紫が<font color='#aa00aa'>静粛教室</font>です!"
     return HttpResponse(content)
